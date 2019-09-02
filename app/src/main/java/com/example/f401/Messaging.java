15eca1d28b831f07a555c419b28dfa9e2ffa9f18
tanuj <saraft7707@gmail.com>
2019-09-02 11:50:08 +0530

    message layout

---
+++ app/src/main/java/com/example/f401/Messaging.java
@@ -0,0 +1,14 @@
+package com.example.f401;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Messaging extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging);
+    }
+}
