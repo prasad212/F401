3b0ba99635ab8882d833bf7c3c07b9314904630c
Prasad Mandge <p.mandge212@gmail.com>
2019-09-02 12:36:35 +0530

    messaging update

--- app/src/main/java/com/example/f401/MainActivity.java
+++ app/src/main/java/com/example/f401/MainActivity.java
@@ -2,14 +2,26 @@ package com.example.f401;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button msgbutton,paymentbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        msgbutton=(Button)findViewById(R.id.msg);
+        paymentbutton=(Button)findViewById(R.id.payment);
+        msgbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),Messaging.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/f401/Messaging.java
+++ app/src/main/java/com/example/f401/Messaging.java
@@ -3,12 +3,15 @@ package com.example.f401;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Messaging extends AppCompatActivity {
-
+    TextView textView1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messaging);
+        textView1=(TextView)findViewById(R.id.text1);
+        textView1.setText("hello");
     }
 }
